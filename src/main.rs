2d30cfceb557556a6b5ddc5aa847e4e2b1e3a040
Steve Robinson <steve.rob@me.com>
2017-04-02 17:22:26 +0530

    basic TCP Listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Back to square one!");
+    let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("received request");
+            }
+            Err(e) => { 
+                println!("Connection failed");
+            }
+        }
+    }
 }
