c0937d092ec50cf3edb88aa3c203fe8786f62c46
Steve Robinson <steve.rob@me.com>
2017-04-03 19:30:39 +0530

    reading from stream before writing to it

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::thread;
+use std::str;
 
 fn respond_hello_world(mut stream: TcpStream) {
+    let mut buffer = [0; 4096];
+    stream.read(&mut buffer).expect("Read failed");
+    println!("Request:");
+    println!("{}", str::from_utf8(&buffer).unwrap());
+
     let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n";
     stream.write(response).expect("Write failed");
 }
